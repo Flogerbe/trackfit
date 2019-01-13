b161ef5fef7039110f6e529a751a278d484d5b8b
George Lucas T. Bentes <george.bentes@gmail.com>
2019-01-13 16:27:43 -0400

    Changes for AddEntry-UdaciSlider

--- components/TrackiSlider.js
+++ components/TrackiSlider.js
@@ -1,10 +1,20 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Slider, StyleSheet, Text } from 'react-native'
 
-export default function TrackiSlider() {
+export default function TrackiSlider({max, unit, step, value, onChange}) {
   return (
     <View>
-      <Text>TrackiSllider</Text>
+      <Slider 
+        step={step}
+        value={value}
+        maximumValue={max}
+        minimumValue={0}
+        onValueChange={onChange}
+      />
+      <View>
+        <Text>{value}</Text>
+        <Text>{unit}</Text>
+      </View>
     </View>
   )
 }
