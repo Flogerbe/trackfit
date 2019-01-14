64c0022aa0fbb02418e9e4175178242b2f1f67db
George Lucas T. Bentes <george.bentes@gmail.com>
2019-01-14 05:07:18 -0400

    Changes for Api

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -6,6 +6,7 @@ import TrackiSteppers from './TrackiSteppers'
 import DateHeader from './DateHeader'
 import { Ionicons } from '@expo/vector-icons'
 import TextButton from './TextButton'
+import { submitEntry, removeEntry } from '../utils/api'
 
 
 function SubmitBtn ({onPress}) {
@@ -66,7 +67,9 @@ export default class AddEntry extends Component {
 
 
     // Navigate to Home
-    // Save to "DB"
+    
+    submitEntry({key, entry})
+
     // Clear local notification
   }
 
@@ -77,7 +80,7 @@ export default class AddEntry extends Component {
 
     // Route to Home
 
-    // Update "DB"
+    removeEntry(key)
   }
 
   render() {
